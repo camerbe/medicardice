dd0fd5f7b8ac368b500852954dea81525c01c9e4
camerbe <tristanvellozzi@gmail.com>
2024-07-04 17:19:33 +0200

    Login && Auth

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
+import {enableProdMode} from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+/*import {enableProdMode} from "@angular/core";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import { AppComponent } from './app/app.component';
+platformBrowserDynamic().bootstrapModule(AppComponent)
+  .catch(err=>console.log(err))*/
