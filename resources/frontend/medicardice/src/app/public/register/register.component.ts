43d536648df3bf271c4ecf715fb5f5044af8127b
camerbe <tristanvellozzi@gmail.com>
2024-08-21 17:19:12 +0200

    Register

--- resources/frontend/medicardice/src/app/public/register/register.component.ts
+++ resources/frontend/medicardice/src/app/public/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -12,6 +12,17 @@ frmGroupRegister!:FormGroup;
   constructor(
     private fb:FormBuilder)
   {
-
+    this.frmGroupRegister=this.fb.group({
+      last_name:['',[Validators.required]],
+      first_name:['',[Validators.required]],
+      email:['',[Validators.required,Validators.email]],
+      role:['Patient'],
+      dob:['',[Validators.required]],
+      phone_number:[''],
+      password:['123456'],
+      password_confirm:['123456'],
+      updated_by:[''],
+      created_by:[''],
+    })
   }
 }
